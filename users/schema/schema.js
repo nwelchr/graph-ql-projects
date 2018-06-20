f30807fab145459b6c7db60cbe4853ff802010a8
nwelchr <nwelchr@gmail.com>
2018-06-20 08:37:43 -0700

    Add to schema file, allow searching for companies and users

---
+++ users/schema/schema.js
@@ -0,0 +1,66 @@
+const graphql = require('graphql');
+const axios = require('axios');
+const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } = graphql;
+
+// GraphQLObjectType: instructs graphQL about the
+// presence of a user
+
+// important to put above
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    // magic
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(resp => resp.data);
+      }
+    }
+  }
+});
+
+// need root query, allows graphQL to jump into our graph of data
+// by taking initial query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    // if you ask me for a user, and give me an ID
+    // I'll give you back a user
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentVaue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(resp => resp.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
